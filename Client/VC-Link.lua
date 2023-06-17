bed3170ed97a7e76fa226c7f5b9c125eb8616f27
decommandpro <93335212+decommandpro@users.noreply.github.com>
2023-06-17 23:45:26 +0200

    Attempt to decrease "hickup" between packets

--- Client/VC-Link.lua
+++ Client/VC-Link.lua
@@ -1,37 +1,41 @@
+local args = { ... }
+os.loadAPI("json")
+local speaker = peripheral.find("speaker")
+if not speaker then error("Unable to Find Speaker") end
+
 local function socket()
-  local i = 0
-  while true do
-    repeat
+    local i = 0
+    while true do
+        repeat
+            i = i + 1
 
-    i=i+1
-
-    local msg = ws.receive()
-    if not msg then break end
-    msg = json.decode(msg)
-    buffer[i] = msg
-
-    until true
-  end
+            local msg = ws.receive()
+            if not msg then break end
+            msg = json.decode(msg)
+            for j, v in pairs(msg) do
+                msg[j] = math.max(math.min(v, 127), -128)
+            end
+            buffer[i] = msg
+        until true
+    end
 end
 
 local function speakerLoop()
-  local i = 0
-  while true do
-    i=i+1
-    repeat sleep() until buffer[i]
-    while not speaker.playAudio(buffer[i], 3000) do
-      os.pullEvent("speaker_audio_empty")
+    local i = 0
+    while true do
+        i = i + 1
+        repeat sleep() until buffer[i]
+        while not speaker.playAudio(buffer[i], 30) do
+            os.pullEvent("speaker_audio_empty")
+        end
     end
-  end
 end
 
 
-os.loadAPI("json")
-speaker = peripheral.find("speaker")
-if not speaker then error("Unable to Find Speaker") end
+
 
 print("Activating VCLink, Attempting to Connect")
-ws, err = http.websocket("ws://decommandpro.dragon-bicolor.ts.net:8082")--ws://7.tcp.eu.ngrok.io:13553")
+ws, err = http.websocket(args[1])
 if not ws then error(err) end
 
 print("Connected to Websocket, Ready and Listening!")
